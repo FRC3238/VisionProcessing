07729c251db6b386406390c47afa28b947283ba2
Clay Molitor <ClayMolitor@live.com>
2014-01-15 19:42:45 -0800

    Improved vision_lib
    added a class called camera for getting pictures from files, webcams,
    and the axis cam.

--- vision_lib.py
+++ vision_lib.py
@@ -4,8 +4,27 @@ import numpy as np
 from cStringIO import StringIO
 
 
-def get_image_from_axis():
+# Used for getting pictures
+class Camera:
+  def __init__(self):
+    self.firstRun = True
+    self.loops = 0
+
+  def from_axis(self):
+      
     url = "http://10.32.38.11/axis-cgi/jpg/image.cgi"
     request = urlopen(url)
     img_array = np.asarray(bytearray(request.read()), dtype=np.uint8)
     return imdecode(img_array, CV_LOAD_IMAGE_COLOR)
+
+  def from_file(self, img_path):
+    return imread(self.img_path)
+
+  def from_webcam(self):
+    if firstRun is True:
+      self.videoCam = VideoCapture(0)
+      firstRun = False
+
+    rval, img = vc.read()
+
+    return img
